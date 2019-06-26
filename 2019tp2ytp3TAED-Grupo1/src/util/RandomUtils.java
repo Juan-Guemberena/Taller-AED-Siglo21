7cbc2993edc8933bbb0af5ac1202b7ed4814343c
Juan-Guemberena <chinoguembe@gmail.com>
2019-06-26 01:44:22 -0300

    Arregle lo del 0 esta vez si

--- 2019tp2ytp3TAED-Grupo1/src/util/RandomUtils.java
+++ 2019tp2ytp3TAED-Grupo1/src/util/RandomUtils.java
@@ -7,23 +7,23 @@ public class RandomUtils {
 
 	private static Random elementosAleatorios = new Random();
 	
-	//Método que genera un elemento del tipo char de forma aleatoria.
+	//M?todo que genera un elemento del tipo char de forma aleatoria.
 	public static char generarElemento() {
 		
 		return (char) (97 + elementosAleatorios.nextInt(26));
-	}//Fin método generarElemento.
+	}//Fin m?todo generarElemento.
 	
-	//Método que genera un numero aleatorio.
+	//M?todo que genera un numero aleatorio.
 	public static int generarNumeroAleatorio() {
 		
-		return elementosAleatorios.nextInt(15);
-	}//Fin método generarNumeroAletorio.
+		return (elementosAleatorios.nextInt(15)+1);
+	}//Fin m?todo generarNumeroAletorio.
 	
 	//Sobrecarga de generarNumerosAleatoriios, genera numeros dentro de un rango determinado.
 	public static int generarNumeroAleatorio(int rango) {
 		
 		return elementosAleatorios.nextInt(rango);
-	}//Fin método generarNumeroAleatorio.
+	}//Fin m?todo generarNumeroAleatorio.
 	
 	//Carga una cola con la cantidad de elementos del parametro cantElementos.
 	public static Box cargarColaRandom(Box caja, int cantElementos) {
@@ -32,5 +32,5 @@ public class RandomUtils {
 		for(int i = 0; i < cantElementos; i++)
 			caja.enqueue(generarElemento());
 		return caja;
-	}//Fin mètodo cargarColaRandom
+	}//Fin m?todo cargarColaRandom
 }//Fin clase RandomUtils.
